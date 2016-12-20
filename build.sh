1dca7b12a8eaae621e70e4bf07166a584d7cf8e9
Simakis Panagiotis <spithas@localhost.localdomain>
2016-12-20 02:23:56 +0200

    debug

--- build.sh
+++ build.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
 OS=$(lsb_release -si)
+
                     #capture the user distro
 
-    if [  $OS = "Fedora" ]; then
+if [  $OS = "Fedora" ]; then
+  sudo yum -y install python-pip python3-pip mpv
+elif [ $OS = "Ubuntu" ] || [ $OS = "Debian" ]; then
+	sudo apt-get --assume-yes install python-pip python3-pip mpv
+elif [$OS = "Arch"]; then
+  sudo pacman -S --yes python2-pip python3-pip mpv
+else
+  echo "We can't install dependencies packages"
+fi
 
-   		sudo yum install python2-pip python3-pip mpv
+clear
+sudo pip2 install menu termcolor
+sudo pip3 install mps-youtube
+clear
+mpsyt set player mpv,q
+clear
 
-    elif [ $OS = "Ubuntu" ] || [ $OS = "Debian" ]; then
+echo "Ready"
 
-  		sudo apt-get install python2-pip python3-pip mpv
+echo "Do you want to start kasetophono_sto_termatiko now? (Y/n)"
+read Choise
 
-    elif [$OS = "Arch"]; then
-      sudo pacman -S python2-pip python3-pip mpv
-    else
-      echo "We can't install dependencies packages"
-    fi
-    clear
-    sudo pip2 install menu termcolor
-    sudo pip3 install mps-youtube
-    clear
-    mpsyt set player mpv,q
-    clear
-    echo "Ready"
-    echo "Do you want to start kasetophono_sto_termatiko now? (Y/n)"
-    read Choise
-    if [ $Choise = "Y" ] || [ $Choise="y" ]; then
-      clear
-      python kasetophono.py
-    else
-      exit
-    fi
-    clear
+if [ $Choise = "Y" ] || [ $Choise="y" ]; then
+  clear
+  python kasetophono.py
+else
+  exit
+fi
+  clear
