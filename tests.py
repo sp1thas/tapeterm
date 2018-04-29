e22bb9ad13e718fad7843d18fdb790c3cc779b45
Simakis Panagiotis <sp1thas@autistici.org>
2018-04-29 23:53:28 +0300

    add more tests

--- tests.py
+++ tests.py
@@ -13,3 +13,12 @@ def test_base_url():
     tl = test_setup()
     tl.get_home()
     test_close(tl)
+
+def test_get_ids():
+    tl = test_setup()
+    tl.get_plid_from_urls()
+    test_close(tl)
+
+def test_tapeterm():
+    from kasetophono import TapeTerm
+    ks = TapeTerm()
