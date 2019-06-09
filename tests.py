92a15a36d4c0e03ce2030d12a3425f918d12b583
Panagiotis Simakis <sp1thas@autistici.org>
2019-06-09 21:25:39 +0300

    add travis configuration and update unittests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+from tapeterm.tapeterm import *
 
 class TestTapeTerm(unittest.TestCase):
 
@@ -10,6 +11,20 @@ class TestTapeTerm(unittest.TestCase):
     def test_json_el_files(self):
         with open('tapeterm/config_en.json', 'r') as f:
             self.assertTrue(bool(json.load(f)))
+
+    def test_tapeterm_en_class_init(self):
+        self.obj = TapeTerm(JSON=JSON_EN)
+        self.assertTrue(isinstance(self.obj, object))
+        self.assertTrue(bool(self.obj.data))
+        self.assertTrue(bool(self.obj.JSON))
+        self.assertTrue(bool(self.obj.mpsyt_bin))
+
+    def test_tapeterm_el_class_init(self):
+        self.obj = TapeTerm(JSON=JSON_EL)
+        self.assertTrue(isinstance(self.obj, object))
+        self.assertTrue(bool(self.obj.data))
+        self.assertTrue(bool(self.obj.JSON))
+        self.assertTrue(bool(self.obj.mpsyt_bin))
 
 
 if __name__ == '__main__':
